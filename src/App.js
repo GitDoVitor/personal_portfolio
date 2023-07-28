82bf3b8f9331bef083a6b1457186ff80cba44004
João Vitor <joaovitorsantoscs@gmail.com>
2023-07-28 02:34:53 -0300

    criado texto de apresentação

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './styles/links.scss';
 import './styles/buttons.scss';
 import Logo from './styles/images/logo.svg';
 import LightSun from './styles/images/light-sun.svg';
+import MyPhoto from './styles/images/IMG_20230218_145511_076 (1).webp';
+import PinMap from './styles/images/pin-map.svg';
 
 const urlDownload = "https://drive.google.com/uc?export=download&id=1oDfoE3KgVX-P-nelLQi9T9Y6TLUe4M16"
 
@@ -10,7 +12,7 @@ function App() {
   return (
     <div className="App">
       <header>
-        <div className="wrapper">
+        <div className="header-wrapper">
           <div className="logo-wrapper">
             <a href='/'>
               <img src={Logo} alt="logo" />
@@ -35,8 +37,32 @@ function App() {
             </div>
           </div>
         </div>
-
       </header>
+      <body>
+        <div className="body-wrapper">
+          <div className="text-wrapper">
+            <span className='pp-h1'>Olá, eu sou o Vitor ✌</span>
+            <p>
+            Engenheiro de software apaixonado por Ciência de Dados. Possuo habilidades em desenvolvimento full stack focado em plataformas low-code. Atualmente estou buscando novas oportunidades para aplicar meus conhecimentos em projetos analíticos desafiadores.
+            </p>
+            <div className='card-wrapper'>
+              <div className='location'>
+                <img src={PinMap} alt="pin-map" />
+                <span className='pp-body-b'>Foz do Iguaçu, Paraná</span>
+              </div>
+              <div className='available'>
+                <div className='dot-wrapper'>
+                  <div className='green-dot'></div>
+                </div>
+                <span className='pp-body-b'>Disponível para novos projetos</span>
+              </div>
+            </div>
+          </div>
+          <div className="image-wrapper">
+            <img src={MyPhoto} alt="foto" className='pp-my-photo' />
+          </div>
+        </div>
+      </body>
     </div>
   );
 }
