b2a21607837940f39068a80328aeb0c59a8bd7b3
João Vitor <joaovitorsantoscs@gmail.com>
2023-07-27 21:33:37 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import './styles/links.scss';
+import './styles/buttons.scss';
+import Logo from './styles/images/logo.svg';
+import LightSun from './styles/images/light-sun.svg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div className="wrapper">
+          <div className="logo-wrapper">
+            <a href='/'>
+              <img src={Logo} alt="logo" />
+            </a>
+          </div>
+
+          <div className="buttons-wrapper">
+            <div className="links-wrapper">
+              <a href='www.google.com' className='pp-link-menu-light'>Sobre mim</a>
+              <a href='www.google.com' className='pp-link-menu-light'>Portfólio</a>
+              <a href='www.google.com' className='pp-link-menu-light'>Contato</a>
+            </div>
+            <div className="social-wrapper">
+              <a href='www.google.com'>
+                <img src={LightSun} alt="light-sun" />
+              </a>
+              <a className='pp-button-light' href='/styles/docs/curriculum.pdf' download>Baixar CV</a>
+            </div>
+          </div>
+        </div>
+
       </header>
     </div>
   );
